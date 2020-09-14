2bd1496edde5eef0f2ef10112b774cb9c863ea29
Jesse <70731243+essej818@users.noreply.github.com>
2020-09-13 21:48:55 -0400

    Edits
    Added a functional slider to home and modules page. Added a placeholder picture for homepage. tweaked html. Tweaked CSS. Added JS for the slides

---
+++ js/js.js
@@ -0,0 +1,27 @@
+//START script for the module slider============================//
+var slideIndex = 1;
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("moduleSlide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" dotActive", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " dotActive";
+}
+//END script for the module slider============================//
