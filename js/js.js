e287caf4e8e5f373201d7014861fd063d6a5aa9f
Jesse <70731243+essej818@users.noreply.github.com>
2020-10-15 21:15:24 -0400

    g

--- js/js.js
+++ js/js.js
@@ -125,6 +125,7 @@ window.onclick = function(event) {
 
 //END script for the reporting page============================//
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 
 //Start script for popup arrow//
@@ -147,3 +148,5 @@ function click() {
 
 
 >>>>>>> a05f261af83c10b67cb457d7a6454647de7b5128
+=======
+>>>>>>> parent of eb92bd8... Merge pull request #25 from INF466/jesse
