2036345e39d4036c0bb18b31ddebbad6527822a1
Alex Lee <rbgod21@gmail.com>
2020-11-12 17:40:45 -0500

    modified

--- js/js.js
+++ js/js.js
@@ -56,94 +56,9 @@ function showSlides(n) {
 }
 //END script for the module slider============================//
 
-//START script for the reporting page=========================//
-
-var activitiesbut = document.getElementById("activitybut");
-activitiesbut.onclick = function () {
-    document.getElementById("formact").classList.add("hide");
-    document.getElementById("formact").classList.remove("hide");
-}
-
-var modal = document.getElementById("modalreport");
-
-var btn = document.getElementById("modalbtn");
-
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-var modal3 = document.getElementById("modalcontact");
-
-var btn3 = document.getElementById("contactbtn");
-
-var span3 = document.getElementsByClassName("close3")[0];
-
-btn3.onclick = function() {
- modal3.style.display = "block";
-}
-
-span3.onclick = function() {
-  modal3.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal3) {
-    modal3.style.display = "none";
-  }
-}
-
-
-var modal2 = document.getElementById("modalreport2");
-
-var btn2 = document.getElementById("modalbtn2");
-
-var span2 = document.getElementsByClassName("close2")[0];
-
-btn2.onclick = function() {
- modal2.style.display = "block";
-}
-
-span2.onclick = function() {
-  modal2.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal2) {
-    modal2.style.display = "none";
-  }
-}
-
-//var expensesbut = document.getElementById("expensebut");
-//expensesbut.onclick = function () {
-//    document.getElementById("formact").classList.add("hide");
-//    document.getElementById("formexp").classList.remove("hide");
-//}
-
-
-//END script for the reporting page============================//
 //START script for accessibility===============================//
 function click() {
 
-}
-
-
-//disclaimer popup
-
-function disclaimer() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
 }
 
 //END script for accessibility=================================//
