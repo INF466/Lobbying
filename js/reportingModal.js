6ca6ed6e4107886d33f4e14a927d6f4994d95be1
Alex Lee <rbgod21@gmail.com>
2020-11-19 19:13:37 -0500

    changed contact information

--- js/reportingModal.js
+++ js/reportingModal.js
@@ -18,3 +18,25 @@ function hideModal(){
 	var modal = document.getElementById("modalPopUp");
 	modal.style.display = "none";
 }
+
+//js for contact & submit//
+
+var modal = document.getElementById("contactModal");
+
+var btn = document.getElementById("contactBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
